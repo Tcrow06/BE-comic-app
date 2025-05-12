89ad023534099461b90bcb052ba294bd31d1662f
congquynguyen296 <congquynguyen296@gmail.com>
2025-05-12 10:23:49 +0700

    Completed api search for story

--- src/main/java/com/nettruyen/comic/controller/StoryController.java
+++ src/main/java/com/nettruyen/comic/controller/StoryController.java
@@ -37,6 +37,14 @@ public class StoryController {
                 .build();
     }
 
+    @GetMapping("/api/story")
+    ApiResponse<List<StoryResponse>> getAllStory() {
+        return ApiResponse.<List<StoryResponse>>builder()
+                .code(200)
+                .result(storyService.getAllStory())
+                .build();
+    }
+
     @PutMapping("/api/story")
     ApiResponse<StoryResponse> updateStory(@RequestBody @Valid StoryUpdateRequest storyUpdateRequest) {
         return ApiResponse.<StoryResponse>builder()
@@ -97,6 +105,16 @@ public class StoryController {
         return ApiResponse.<Map<String, Object>>builder()
                 .code(200)
                 .result(response)
+                .build();
+    }
+
+    // Tìm kiếm theo code truyện
+    @GetMapping("/api/story/search")
+    ApiResponse<List<StoryResponse>> getStoryByCode(@RequestParam("code") String code) {
+        return ApiResponse.<List<StoryResponse>>builder()
+                .code(200)
+                .result(storyService.getAllStoryByGenerateCode(code))
+                .message("Get completed")
                 .build();
     }
 
